dbdeaba701660b993e02f4835a2f2a0cf273a66b
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2017-02-16 10:06:15 +0200

    Home controller added.

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,13 @@ Bundler.require(*Rails.groups)
 
 module DnsHelper
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.controller_specs false
+      g.view_specs false
+      g.helper false
+      g.assets false
+      g.decorator false
+      g.skip_routes  true
+    end
   end
 end
